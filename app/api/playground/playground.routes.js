8bffcee0ff3bede280907fe95086ce371bd5dd61
exMachina <shyampadia@live.com>
2018-05-01 14:43:49 -0400

    playground added

---
+++ app/api/playground/playground.routes.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const router = express.Router();
+
+// should come from model
+var foods = [
+    { "id": 1, "name": "Donuts" },
+    { "id": 2, "name": "Pizza" },
+    { "id": 3, "name": "Tacos" }
+  ];
+   
+  var books = [
+    { "title": "Hitchhiker's Guide to the Galaxy" },
+    { "title": "The Fellowship of the Ring" },
+    { "title": "Moby Dick" }
+  ];
+   
+  var movies = [
+    { "title": "Ghostbusters" },
+    { "title": "Star Wars" },
+    { "title": "Batman Begins" }
+  ];
+
+router.get('/', function(req, res) {
+    res.send("THIS IS playground");
+});
+
+// the GET "books" API endpoint
+router.get('/api/books', function (req, res) {
+    res.send(books);
+});
+ 
+// the GET "movies" API endpoint
+router.get('/api/movies', function (req, res) {
+    res.send(movies);
+});
+ 
+// the GET "foods" API endpoint
+router.get('/api/food', function (req, res) {
+    res.send(foods);
+});
+
+// POST endpoint for creating a new food
+router.post('/api/food', function (req, res) {
+    // calculate the next ID
+    let id = 1;
+    if (foods.length > 0) {
+        let maximum = Math.max.apply(Math, foods.map(function (f) { return f.id; }));
+        id = maximum + 1;
+    }
+    console.log("req body", req.body);
+    let new_food = {"id": id, "name": req.body.name};
+    foods.push(new_food);
+    res.send(new_food);
+});
+
+// PUT endpoint for editing food
+router.put('/api/food/:id', function (req, res) {
+    let id = req.params.id;
+    let f = foods.find(x => x.id == id);
+    f.name = req.body.name;
+    res.send(f);
+});
+ 
+// DELETE endpoint for deleting food
+router.delete('/api/food/:id', function (req, res) {
+    let id = req.params.id;
+    let f = foods.find(x => x.id == id);
+    foods = foods.filter(x => x.id != id);
+    res.send(f);
+});
+
+module.exports = router
--- app/api/routes.js
+++ app/api/routes.js
@@ -4,8 +4,11 @@ const router = express.Router();
 // all routers
 const userRouter = require('./users/users.routes');
 const aboutRouter = require('./about/about.routes');
+const playgroundRouter = require('./playground/playground.routes');
 
 router.use('/users', userRouter);
 router.use('/about', aboutRouter);
+router.use('/playground', playgroundRouter);
+
 
 module.exports = router
